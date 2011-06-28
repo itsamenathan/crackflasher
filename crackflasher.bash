5aacb4a7f1f74b079e7e1600536f7d0ecd1fc530
Nathan W <nathan@frcv.net>
2011-06-28 14:58:57 -0500

    - adb root is called before pushing things to /cache/recovery - moved the push extendedcommand command after the final review     this way you are saying yes, then things happen instead of before. - added a -d option for device id, this will make it so that adb only send commands to that device. - added a little ascii art - added the link to git hub

--- crackflasher.bash
+++ crackflasher.bash
@@ -3,11 +3,12 @@
 ###########################################################################
 #                 CONFIG
 # Location of adb
-ADB="/home/nwarner/Desktop/android-sdk-linux_x86/platform-tools/adb"
+ADB="/home/jimshoe/android-sdk-linux_86/platform-tools/adb"
 ###########################################################################
 
 # flags
 BACKUP=0
+DEVICE=0
 WIPE=0
 RECOVER=0
 LIST=0
@@ -20,6 +21,7 @@ GREEN="\033[32m"
 
 if [ $# -eq 0 ]; then
   echo "Usage: ./crackflasher -b -w [num] update.zip update2.zip"
+  echo "-d - device id from adb devices, this will tell this script to send adb commands to only that device"
   echo "-w - wipe Dalvik /cache /data it is optional [num] specifies how many wipes to do"
   echo "-b - makes backup in /sdcard/clockworkmod/backup/ named date and is optional"
   echo "-l - list backups in /sdcard/clockworkmod/backup/"
@@ -28,24 +30,14 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-if [ `ps -ef | grep -v "grep" | grep -c "adb fork-server"` -lt 1 ]; then
-  echo "ADB server isn't running"
-  exit 1
-fi
-
-if [ `$ADB devices | wc -l ` -lt 3 ]; then
-  echo "Couldn't find device"
-  exit 1
-fi
-
-if [ -f $EXTCOMMANDS ]; then
-  rm $EXTCOMMANDS
-fi
-
-while getopts ":w:br:l" opt; do
+while getopts ":w:br:ld:" opt; do
   case $opt in
     b)
       BACKUP=1
+      ;;
+    d)
+      DEVICE=$OPTARG
+      ADB=$ADB" -s $OPTARG" 
       ;;
     w)
       if [ $OPTARG -eq $OPTARG 2> /dev/null ]; then
@@ -72,6 +64,58 @@ while getopts ":w:br:l" opt; do
   esac
 done
 
+if [ `ps -ef | grep -v "grep" | grep -c "adb fork-server"` -lt 1 ]; then
+  echo "ADB server isn't running"
+  exit 1
+fi
+
+NUM_DEVICES=`$ADB devices | wc -l `
+if [ $NUM_DEVICES -lt 3 ]; then
+  echo "Couldn't find device"
+  exit 1
+fi
+
+if [ $NUM_DEVICES -gt 3 ] && [ $DEVICE == 0 ]; then
+  echo -e "\n$BOLD$RED"
+  echo -n "You will need to use the -d option with one of the following device IDs"
+  echo -e "\n$NORM"
+  $ADB devices 
+  exit 1
+fi
+
+if [ `$ADB get-state` == "unknown" ]; then
+  echo -e "\n$BOLD$RED"
+  echo -n "Device is in an unknown state.  Are you sure you have the correct ID?"
+  echo -e "\n$NORM"
+  exit 1
+fi
+
+if [ -f $EXTCOMMANDS ]; then
+  rm $EXTCOMMANDS
+fi
+
+
+echo "ui_print(\"_______  _______  _______  _______  _         \");" >> $EXTCOMMANDS
+echo "ui_print(\"(  ____ \(  ____ )(  ___  )(  ____ \| \    /\ \");" >> $EXTCOMMANDS
+echo "ui_print(\"| (    \/| (    )|| (   ) || (    \/|  \  / / \");" >> $EXTCOMMANDS
+echo "ui_print(\"| |      | (____)|| (___) || |      |  (_/ /  \");" >> $EXTCOMMANDS
+echo "ui_print(\"| |      |     __)|  ___  || |      |   _ (   \");" >> $EXTCOMMANDS
+echo "ui_print(\"| |      | (\ (   | (   ) || |      |  ( \ \  \");" >> $EXTCOMMANDS
+echo "ui_print(\"| (____/\| ) \ \__| )   ( || (____/\|  /  \ \ \");" >> $EXTCOMMANDS
+echo "ui_print(\"(_______/|/   \__/|/     \|(_______/|_/    \/ \");" >> $EXTCOMMANDS
+echo "ui_print(\"                                              \");" >> $EXTCOMMANDS
+echo "ui_print(\"                CrackFlasher                  \");" >> $EXTCOMMANDS
+echo "ui_print(\"  https://github.com/JimShoe/crackflasher     \");" >> $EXTCOMMANDS
+echo "ui_print(\"_______  _        _______  _______            \");" >> $EXTCOMMANDS
+echo "ui_print(\"(  ____ \( \      (  ___  )(  ____ \|\     /| \");" >> $EXTCOMMANDS
+echo "ui_print(\"| (    \/| (      | (   ) || (    \/| )   ( | \");" >> $EXTCOMMANDS
+echo "ui_print(\"| (__    | |      | (___) || (_____ | (___) | \");" >> $EXTCOMMANDS
+echo "ui_print(\"|  __)   | |      |  ___  |(_____  )|  ___  | \");" >> $EXTCOMMANDS
+echo "ui_print(\"| (      | |      | (   ) |      ) || (   ) | \");" >> $EXTCOMMANDS
+echo "ui_print(\"| )      | (____/\| )   ( |/\____) || )   ( | \");" >> $EXTCOMMANDS
+echo "ui_print(\"|/       (_______/|/     \|\_______)|/     \| \");" >> $EXTCOMMANDS
+echo "ui_print(\"                                              \");" >> $EXTCOMMANDS
+echo "ui_print(\"                                              \");" >> $EXTCOMMANDS
 
 if [ $BACKUP -eq 1 ]; then
   backup_file=`date +%Y-%m-%d.%H.%M.%S`
@@ -138,10 +182,19 @@ if [ $LIST -eq 1 ]; then
   exit
 fi
 
-$ADB push $EXTCOMMANDS /cache/recovery/ 2> /dev/null
 
 echo -e "\n$BOLD$RED"
 read -n1 -p "REBOOT & INSTALL? (y/N) [N] "
+echo -e "\n$NORM"
 echo
-[[ $REPLY = [yY] ]] && $ADB reboot recovery || { echo "Aborting!."; exit 1; }
+if [[ $REPLY = [yY] ]]; then
+  if [ `$ADB root | grep -c "already running as root"` -ne 1 ]; then
+    sleep 5
+  fi
+  $ADB push $EXTCOMMANDS /cache/recovery/ 2> /dev/null
+  $ADB reboot recovery
+else
+  echo "Aborting!."
+  exit 1;
+fi
 
