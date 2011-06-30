98910d9af9e7fcafa7615dff35ed705d910f33fb
Nathan W <nathan@frcv.net>
2011-06-30 15:15:46 -0500

    removed running /cache/dowipedalvikcache.sh this is   useless when wiping cache, data, and sd-ext added a -q option for booting into bootloader added a -s option for formatting /system

--- crackflasher.bash
+++ crackflasher.bash
@@ -12,6 +12,8 @@ DEVICE=0
 WIPE=0
 RECOVER=0
 LIST=0
+BOOTLOADER=0
+WIPE_SYSTEM=0
 
 EXTCOMMANDS="/tmp/extendedcommand"
 BOLD="\033[1m"
@@ -21,16 +23,18 @@ GREEN="\033[32m"
 
 if [ $# -eq 0 ]; then
   echo "Usage: ./crackflasher -b -w [num] update.zip update2.zip"
-  echo "-d - device id from adb devices, this will tell this script to send adb commands to only that device"
-  echo "-w - wipe Dalvik /cache /data it is optional [num] specifies how many wipes to do"
   echo "-b - makes backup in /sdcard/clockworkmod/backup/ named date and is optional"
+  echo "-d - device id from adb devices, this will tell this script to send adb commands to only that device"
   echo "-l - list backups in /sdcard/clockworkmod/backup/"
+  echo "-q - boot into bootloader if available"
   echo "-r - recovery from backup. If backup is on local machine it will push it to /sdcard/clockworkmod/backup/"
+  echo "-s - wipe system when using -w.  USE ONLY IF YOU KNOW WHAT YOU ARE DOING"
+  echo "-w - wipe Dalvik /cache /data it is optional [num] specifies how many wipes to do"
   echo "list in order the update.zip files you want to install"
   exit 1
 fi
 
-while getopts ":w:br:ld:" opt; do
+while getopts ":w:br:ld:qs" opt; do
   case $opt in
     b)
       BACKUP=1
@@ -52,6 +56,12 @@ while getopts ":w:br:ld:" opt; do
       ;;
     l)
       LIST=1
+      ;;
+    q)
+      BOOTLOADER=1
+      ;;
+    s)
+      WIPE_SYSTEM=1
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -83,7 +93,13 @@ if [ $NUM_DEVICES -gt 3 ] && [ $DEVICE == 0 ]; then
   exit 1
 fi
 
-if [ `$ADB get-state` == "unknown" ]; then
+if [ $NUM_DEVICES -gt 3 ] && [ $DEVICE != 0 ]; then
+  DEVICE_STATE=`adb devices | grep -v attached | grep $DEVICE | awk '{print $2}'`
+else
+  DEVICE_STATE=`adb devices | grep -v attached | awk '{print $2}'`
+fi
+
+if [ $DEVICE_STATE == "unknown" ]; then
   echo -e "\n$BOLD$RED"
   echo -n "Device is in an unknown state.  Are you sure you have the correct ID?"
   echo -e "\n$NORM"
@@ -124,12 +140,17 @@ if [ $BACKUP -eq 1 ]; then
 fi
 
 for ((i=0; i < $WIPE; i++)) {
-  echo "run_program(\"/cache/dowipedalvikcache.sh\");" >> $EXTCOMMANDS
   echo "format(\"/cache\");" >> $EXTCOMMANDS
   echo "format(\"/data\");" >> $EXTCOMMANDS
   echo "format(\"/sd-ext\");" >> $EXTCOMMANDS
   echo "format(\"/sdcard/.android_secure\");" >> $EXTCOMMANDS
-  echo -e "${BOLD}${RED}FORMAT${NORM} Dalvik, /cache, /data, /sd-ext"
+  if [ $WIPE_SYSTEM -eq 1 ]; then
+    echo "format(\"/system\");" >> $EXTCOMMANDS
+    echo -e "${BOLD}${RED}FORMATTING${NORM} Dalvik, /cache, /data, /sd-ext"
+    echo -e "${BOLD}${RED}FORMATTING SYSTE!!!${NORM}"
+  else
+    echo -e "${BOLD}${RED}FORMAT${NORM} Dalvik, /cache, /data, /sd-ext"
+  fi
 }
 
 
@@ -140,10 +161,10 @@ if [ $RECOVER != 0 ]; then
     else
       filename=`basename $RECOVER`
       if [ `$ADB shell stat /sdcard/clockworkmod/backup/$filename | grep -c "can't stat"` -eq 0 ]; then
-        read -n1 -p "Backup already exists on sdcard, overwrite? (y/N) [N] "
+        echo "Pushing $filename to sdcard" 
+        read -n1 -p "$filename already exists on sdcard, overwrite? (y/N) [N] "
         if [[ $REPLY = [yY] ]]; then
           echo
-          echo "Pushing $filename to sdcard" 
           $ADB shell rm -r /sdcard/clockworkmod/backup/$filename
           $ADB shell mkdir /sdcard/clockworkmod/backup/$filename
           $ADB push $RECOVER /sdcard/clockworkmod/backup/$filename 2> /dev/null
@@ -158,10 +179,10 @@ else
     fullpath=${args[$i]}
     filename=`basename ${args[$i]}`
     if [ `$ADB shell stat /sdcard/$filename | grep -c "can't stat"` -eq 0 ]; then
-      read -n1 -p "Update already exists on sdcard, overwrite? (y/N) [N] "
+      echo "Pushing $filename to /sdcard"
+      read -n1 -p "$filename already exists on sdcard, overwrite? (y/N) [N] "
       if [[ $REPLY = [yY] ]]; then
         echo
-        echo "Pushing $filename to /sdcard"
         $ADB shell rm -r /sdcard/$filename
         $ADB push $fullpath /sdcard/ 2> /dev/null
       fi
@@ -182,6 +203,10 @@ if [ $LIST -eq 1 ]; then
   exit
 fi
 
+if [ $BOOTLOADER -eq 1 ]; then
+  echo -e "${BOLD}${RED}REBOOT BOOTLOADER${NORM} Going to reboot into bootloader when complete"
+  echo "run_program(\"/sbin/reboot\",\"bootloader\");" >> $EXTCOMMANDS
+fi
 
 echo -e "\n$BOLD$RED"
 read -n1 -p "REBOOT & INSTALL? (y/N) [N] "
